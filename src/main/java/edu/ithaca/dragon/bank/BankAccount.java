1a53c0105a93488972cd70273bf53b729251b72e
Mark Volkov <markvolkov@Marks-MacBook-Pro.local>
2019-01-29 21:32:09 -0500

    Working on isAmountValid functionality

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -18,11 +18,12 @@ public class BankAccount {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
         }
 
-        if (startingBalance >= 0) {
+        if (isAmountValid(startingBalance)) {
             this.balance = startingBalance;
         } else {
             throw new IllegalArgumentException("Starting balance cannot be negative: " + startingBalance);
         }
+
     }
 
     public double getBalance(){
@@ -37,15 +38,39 @@ public class BankAccount {
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
     public void withdraw (double amount)  throws InsufficientFundsException {
+        if (isAmountValid(amount)) throw new IllegalArgumentException("Invalid amount, valid amounts are greater than 0, and have no more than 2 decimal places");
         if (amount < 0 || amount > balance) throw new InsufficientFundsException("Insufficient funds.");
         balance -= amount;
 
     }
 
+    /***
+     * Problems with amounts
+     * No money amounts coming in should have more than 2 decimal places. Also should never be negative. We need to check this in multiple places...
+     * Write the header and comment for a static method called isAmountValid that takes a double and returns true if the amount is positive and has two decimal points or less, and false otherwise. Make the method always return false for now (Do not even bother to think about how you will write this function).
+     * Write tests that check isAmountValid works to specification (some will fail now).
+     * git add, commit, push
+     * fix the function to meet the specification
+     * git add, commit, push
+     * Add tests to the constructorTest and the withdrawTest that check that negative and many decimal place numbers can't be given (these tests will fail).
+     * git add, commit, push
+     * Fix constructor and withdraw to rely on isAmountValid
+     * git add, commit, push
+     * */
+
+
+    /**
+     * @param amount
+     * @return true if the amount is positive and has two decimal points or less, false otherwise
+     */
+    public static boolean isAmountValid(double amount) {
+        return false;
+    }
+
     public static boolean isEmailValid(String email) {
         if (email.length() == 0) return false;
         if (email.indexOf('@') == -1) return false;
-        Pattern pattern = Pattern.compile("\\w+@{1}\\w+.\\w+");
-        return pattern.matcher(email).matches();
+        return Pattern.compile("\\w+@{1}\\w+.\\w+").matcher(email).matches();
     }
+
 }
--- src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dragon/bank/BankAccountTest.java
@@ -17,6 +17,8 @@ class BankAccountTest {
     void withdrawTest() throws InsufficientFundsException {
         BankAccount bankAccount = new BankAccount("a@b.com", 200);
         bankAccount.withdraw(100);
+        bankAccount.withdraw(-100);
+        bankAccount.withdraw(100.001);
 
         assertEquals(100, bankAccount.getBalance());
     }
@@ -33,15 +35,16 @@ class BankAccountTest {
 
     @Test
     void constructorTest() {
-        BankAccount bankAccount = new BankAccount("a@b.com", 200);
+        BankAccount bankAccount = new BankAccount("a@b.com", 300);
 
         assertEquals("a@b.com", bankAccount.getEmail());
-        assertEquals(200, bankAccount.getBalance());
+        assertEquals(300, bankAccount.getBalance());
         //check for exception thrown correctly
         //Invalid Email
         assertThrows(IllegalArgumentException.class, ()-> new BankAccount("", 100));
         //Invalid Balance
         assertThrows(IllegalArgumentException.class, ()-> new BankAccount("valid@valid.com", -100));
+        assertThrows(IllegalArgumentException.class, ()-> new BankAccount("valid@valid.com", 100.001));
     }
 
 }
